b731091d9e7ebe7bc18b05002b82160e15bb6e93
Harath24 <lryab@rambler.ru>
2021-01-17 15:44:24 +0300

    refactor FormControl

--- src/components/common/formsControl/FormsControl.jsx
+++ src/components/common/formsControl/FormsControl.jsx
@@ -3,15 +3,10 @@ import style from './FormsControl.module.css'
 
 export const FormControl = ({input, meta, ...props}) => {
     const hasError = meta.touched && meta.error
-    const SpecialTeg = () => {
-        if(props.typefield === 'textarea'){ return <textarea {...input} {...props} />
-        }else if(props.typefield === 'input') {return <input {...input} {...props} />}
-        else if(props.typefield === 'checkbox') return <input {...input} {...props} type='checkbox'/>
-    }
     return (
         <div className={style.formControl + ' ' + (hasError ? style.errors : '')}>
             <div>
-                <SpecialTeg />
+                {(props.typefield !== 'checkbox' && <props.typefield {...input} {...props} />) || <input type='checkbox'/>}
             </div>
             <div>
                 {hasError && <span>{meta.error}</span>}
