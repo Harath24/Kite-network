196567ae3d0fef5b85eabd0a6b06444a1192f3ad
Harath24 <lryab@rambler.ru>
2021-01-28 11:30:34 +0300

    React.lazy / Suspense

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
 import {Route} from 'react-router-dom'
-import News from "./components/news/News";
-import Music from "./components/music/Music";
-import Settings from "./components/settings/Settings";
-import DialogsContainer from "./components/dialogs/DialogsContainer";
-import NavbarContainer from "./components/navbar/NavbarContainer";
-import UsersContainer from "./components/users/UsersContainer";
-import ProfileContainer from "./components/profile/ProfileContainer";
 import HeaderContainer from "./components/header/HeaderContainer";
-import Login from "./components/login/Login";
+import NavbarContainer from "./components/navbar/NavbarContainer";
+//import News from "./components/news/News";
+//import Music from "./components/music/Music";
+//import Settings from "./components/settings/Settings";
+//import DialogsContainer from "./components/dialogs/DialogsContainer";
+//import UsersContainer from "./components/users/UsersContainer";
+//import ProfileContainer from "./components/profile/ProfileContainer";
+//import Login from "./components/login/Login";
 import {connect} from "react-redux";
-import {withRouter} from "react-router";
+import {Redirect, withRouter} from "react-router";
 import {compose} from "redux";
 import {initializeApp} from "./state/appReducer";
 import Preloader from "./components/common/preloader/preloader";
+
+const ProfileContainer = React.lazy(() => import('./components/profile/ProfileContainer'));
+const DialogsContainer = React.lazy(() => import('./components/dialogs/DialogsContainer'));
+const UsersContainer = React.lazy(() => import('./components/users/UsersContainer'));
+const Login = React.lazy(() => import('./components/login/Login'));
+const News = React.lazy(() => import('./components/news/News'));
+const Music = React.lazy(() => import('./components/music/Music'));
+const Settings = React.lazy(() => import('./components/settings/Settings'));
 
 class App extends React.Component {
 
@@ -30,16 +38,19 @@ class App extends React.Component {
             <div className='app-wrapper'>
                 <HeaderContainer/>
                 <NavbarContainer/>
+                <Suspense fallback={<div><Preloader/>Загрузка...</div>}>
                 <div className='app-wrapper-content'>
                     <Route path={'/profile/:userId?'} render={() => <ProfileContainer/>}/>
                     <Route path={'/dialogs'} render={() => <DialogsContainer/>}/>
                     <Route path={'/users'} render={() => <UsersContainer/>}/>
                     <Route path={'/login'} render={() => <Login/>}/>
-                    <Route path={'/news'} render={News}/>
-                    <Route path={'/music'} render={Music}/>
-                    <Route path={'/settings'} render={Settings}/>
+                    <Route path={'/news'} render={() => <News/>}/>
+                    <Route path={'/music'} render={() => <Music/>}/>
+                    <Route path={'/settings'} render={() => <Settings/>}/>
                     {/*<Route path={'/friends'} render={() => <Friends/>}/>*/}
                 </div>
+                </Suspense>
+                <Redirect from='/' to='/login'/>
             </div>
         );
     }
